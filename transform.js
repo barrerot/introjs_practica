7b34c3f252206cade9f880f356b3fb1892aa0ade
Carlos Barrero Martínez <barrerot@gmail.com>
2023-08-03 14:22:27 +0200

    ejerciciotransformacion

---
+++ transform.js
@@ -0,0 +1,44 @@
+// transform.js
+
+const datos = [
+    {
+      id: 1,
+      nombre: 'Juan',
+      habilidades: ['JavaScript', 'HTML', 'CSS'],
+      proyectos: [
+        { id: 1, nombre: 'Proyecto 1' },
+        { id: 2, nombre: 'Proyecto 2' }
+      ]
+    },
+    {
+      id: 2,
+      nombre: 'María',
+      habilidades: ['Python', 'SQL', 'Django'],
+      proyectos: [
+        { id: 3, nombre: 'Proyecto 3' },
+        { id: 4, nombre: 'Proyecto 4' }
+      ]
+    },
+    {
+      id: 3,
+      nombre: 'Pedro',
+      habilidades: ['Java', 'Spring', 'Hibernate'],
+      proyectos: [
+        { id: 5, nombre: 'Proyecto 5' },
+        { id: 6, nombre: 'Proyecto 6' }
+      ]
+    }
+  ];
+  
+  // Filtrar desarrolladores que tienen "JavaScript" como habilidad
+  const desarrolladoresJavascript = datos.filter(desarrollador => desarrollador.habilidades.includes('JavaScript'));
+  
+  // Obtener los proyectos en los que trabajan los desarrolladores
+  const proyectosDesarrolladores = desarrolladoresJavascript.reduce((proyectos, desarrollador) => {
+    proyectos.push(...desarrollador.proyectos.map(proyecto => proyecto.nombre));
+    return proyectos;
+  }, []);
+  
+  console.log("Desarrolladores con habilidad JavaScript:", desarrolladoresJavascript);
+  console.log("Proyectos en los que trabajan los desarrolladores JavaScript:", proyectosDesarrolladores);
+  
