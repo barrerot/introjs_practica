d41171420547a82cc155c74faf09945b413442bc
Carlos Barrero Martínez <barrerot@gmail.com>
2023-08-03 13:31:19 +0200

    ejercicio1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ejercicio 1
+# Ejercicio 1: He añadido todos los módulos que componen el temario
 # Ejercicio 2 Arreglar bug
 # Ejercicio 3 Transformaciones
 Ejercicio 4 Arreglar bug de asincronia
---
+++ ejercicio1.js
@@ -0,0 +1,16 @@
+// ejercicio1.js
+
+const persona = {
+  nombre: "Carlos",
+  apellidos: "Barrero Martínez",
+  temasConocidos: [
+
+    " Web 101","Git & Github","Fundamentos de WebHTML5, CSS3","Introducción a JavaScript","modelado de Datos e introducción a SQL","Desarrollo backend con Node.js","Desarrollo Frontend con JavaScript","Frontend PRO","Web Components","Fundamentos de React"," Testing con JavaScript","Desarrollo Backend Avanzadocon Node.js","React Avanzado","Configuración de servidores",],
+  busquedaActiva: false, // Estoy de baja temporal prorrogada pero con contrato de trabajo
+  redesSociales: [
+    {
+      nombre: "Github",
+      link: "https://github.com/barrerot",
+    },
+  ],
+};
